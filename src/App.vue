555f54ab0ae21793cccccf726afcd2fca30a23d4
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-02-05 15:33:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'Pokèvuex',
+}
+</script>
+
+<template>
+  <h1 class="text-center my-5">prova</h1>
+</template>
+
+<style lang="scss" scoped>
+@use './assets/scss/style.scss';
+</style>
