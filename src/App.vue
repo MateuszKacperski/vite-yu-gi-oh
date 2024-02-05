4537e28906cb021e8669c2e2fb91ab0f79d5893a
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-02-05 17:18:52 +0100

    added store

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script>
+import axios from 'axios';
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons'
+import { store } from './data/store.js';
 import AppMain from './components/AppMain.vue'
 export default {
   name: 'Pokèvuex',
   components: {
     AppMain
+  },
+  created() {
+    axios.get(endpoint).then(res => {
+      store.pokemons = res.data.docs;
+    })
   }
 }
 </script>
--- src/components/AppPokemonList.vue
+++ src/components/AppPokemonList.vue
@@ -1,26 +1,20 @@
 <script>
-import axios from 'axios';
-const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons'
+import { store } from '../data/store.js';
 import AppPokemonCard from './AppPokemonCard.vue'
 export default {
     name: 'AppPokemonList',
     data: () => ({
-        pokemons: []
+        store
     }),
     components: {
         AppPokemonCard
-    },
-    created() {
-        axios.get(endpoint).then(res => {
-            this.pokemons = res.data.docs;
-        })
     }
 }
 </script>
 <template>
     <div class="my-5">
         <div class="row g-3 row-cols-3">
-            <div class="col" v-for="pokemon in pokemons" :key="pokemon.id">
+            <div class="col" v-for="pokemon in store.pokemons" :key="pokemon.id">
                 <AppPokemonCard :name="pokemon.name" :number="pokemon.number" :type="pokemon.type1"
                     :image="pokemon.imageUrl" />
             </div>
