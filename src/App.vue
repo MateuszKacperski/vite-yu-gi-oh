f475e809520855a39ad716f644e919fc9a866a82
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-02-05 15:49:15 +0100

    added all components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script>
+import AppMain from './components/AppMain.vue'
 export default {
   name: 'Pokèvuex',
+  components: {
+    AppMain
+  }
 }
 </script>
 
 <template>
-  <h1 class="text-center my-5">prova</h1>
+  <header>
+    <h1 class="text-center my-5">Pokèvuex</h1>
+  </header>
+  <AppMain />
 </template>
 
 <style lang="scss" scoped>
