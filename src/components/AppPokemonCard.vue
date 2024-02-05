bbfe79b2c3131be524bac76ea2234d0e5a99d5ce
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-02-05 16:28:46 +0100

    added dinamic pokemons

--- src/components/AppPokemonCard.vue
+++ src/components/AppPokemonCard.vue
@@ -1,7 +1,24 @@
 <script>
 export default {
     name: 'AppPokemonCard',
+    props: {
+        number: Number,
+        name: String,
+        type: String,
+        image: String
+    }
 }
 </script>
-<template></template>
-<style lang="scss"></style>
+<template>
+    <div class="pokemon-card text-center p-2">
+        <img :src="image" :alt="name" class="img-fluid rounded-circle mb-5">
+        <h4>{{ number }}</h4>
+        <div><strong>{{ name }}</strong></div>
+        <div>{{ type }}</div>
+    </div>
+</template>
+<style lang="scss">
+img {
+    max-height: 150px;
+}
+</style>
--- src/components/AppPokemonList.vue
+++ src/components/AppPokemonList.vue
@@ -1,15 +1,31 @@
 <script>
+import axios from 'axios';
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons'
 import AppPokemonCard from './AppPokemonCard.vue'
 export default {
     name: 'AppPokemonList',
+    data: () => ({
+        pokemons: []
+    }),
     components: {
         AppPokemonCard
+    },
+    created() {
+        axios.get(endpoint).then(res => {
+            this.pokemons = res.data.docs;
+        })
     }
 }
 </script>
 <template>
-    <div>
-        <AppPokemonCard />
+    <div class="my-5">
+        <div class="row g-3 row-cols-3">
+            <div class="col" v-for="pokemon in pokemons" :key="pokemon.id">
+                <AppPokemonCard :name="pokemon.name" :number="pokemon.number" :type="pokemon.type1"
+                    :image="pokemon.imageUrl" />
+            </div>
+        </div>
+
     </div>
 </template>
 <style lang="scss"></style>
