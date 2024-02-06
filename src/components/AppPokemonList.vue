d540c0afdeb4e4a5ece7b92058aef4aeb0877d72
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-02-06 12:49:00 +0100

    fixing

--- src/components/AppPokemonList.vue
+++ src/components/AppPokemonList.vue
@@ -14,7 +14,7 @@ export default {
 <template>
     <div class="my-5">
         <div class="row g-3 row-cols-3">
-            <div class="col" v-for="pokemon in store.pokemons" :key="pokemon.id">
+            <div class="col" v-for="pokemon in store.pokemons" :key="pokemon._id">
                 <AppPokemonCard :name="pokemon.name" :number="pokemon.number" :type="pokemon.type1"
                     :image="pokemon.imageUrl" />
             </div>
